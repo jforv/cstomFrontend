8bc3169d963bbbe363f12e11325974534d53a83c
jforv <xbrain1@gmail.com>
2019-10-10 12:52:15 -0400

    fix semicolon error with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "semi": [2, "never"],
   }
 }
