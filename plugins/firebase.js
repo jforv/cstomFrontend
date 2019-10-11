4e5f72d096ac253fddfda8f215e431d3adfbb283
jforv <xbrain1@gmail.com>
2019-10-11 14:14:51 -0400

    fix security risk

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/auth'
 import 'firebase/database'
 
 var config = {
-apiKey: "AIzaSyApn1yDyoWp9MG_eWtybja4SB5PR-Z-gzw",
-authDomain: "graceclinic-97af9.firebaseapp.com",
-databaseURL: "https://graceclinic-97af9.firebaseio.com",
-projectId: "graceclinic-97af9",
-storageBucket: "graceclinic-97af9.appspot.com",
-messagingSenderId: "554552617490",
-appId: "1:554552617490:web:147a4b4a027315ab6b46cb"
+
 }
 !firebase.apps.length ? firebase.initializeApp(config) : ''
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
